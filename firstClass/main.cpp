fdcc7e7745f69f0fe8c39a8c5b896d1828fecd1a
erlosshex <gugsabr@gmail.com>
2016-09-30 00:32:51 +0800

    first program

---
+++ firstClass/main.cpp
@@ -0,0 +1,23 @@
+#include <windows.h>
+#include <GL/glut.h>
+
+void myDisplay()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glRectf(-0.5f,-0.5f,0.5f,0.5f);
+    glFlush();
+}
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_RGB|GLUT_SINGLE);
+
+    glutInitWindowPosition(100,100);
+    glutInitWindowSize(400,400);
+    glutCreateWindow("first OpenGL program");
+
+    glutDisplayFunc(&myDisplay);
+    glutMainLoop();
+    return 0;
+}
